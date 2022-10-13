5e6fb3042c6f119d8000aa486d9ce78a921ecd84
Heri Setyawan <herisetyawan233@gmail.com>
2022-10-13 12:57:16 +0700

    fix navigation error

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,7 +76,7 @@
               <!-- body -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="px-3 py-2 rounded mr-3 bg-gray-100 border border-gray-300">
                   <i class="fad fa-birthday-cake text-sm"></i>
@@ -97,7 +97,7 @@
               <!-- end item -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="px-3 py-2 rounded mr-3 bg-gray-100 border border-gray-300">
                   <i class="fad fa-user-circle text-sm"></i>
@@ -118,7 +118,7 @@
               <!-- end item -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="px-3 py-2 rounded mr-3 bg-gray-100 border border-gray-300">
                   <i class="fad fa-images text-sm"></i>
@@ -139,7 +139,7 @@
               <!-- end item -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="px-3 py-2 rounded mr-3 bg-gray-100 border border-gray-300">
                   <i class="fad fa-alarm-exclamation text-sm"></i>
@@ -196,7 +196,7 @@
               <!-- body -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
                   <img class="w-full h-full object-cover" src="img/user1.jpg" alt="">
@@ -217,7 +217,7 @@
               <!-- end item --> 
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
                   <img class="w-full h-full object-cover" src="img/user2.jpg" alt="">
@@ -238,7 +238,7 @@
               <!-- end item -->
   
               <!-- item -->
-              <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
+              <a class="flex flex-row items-center justify-start px-4 py-4 capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
   
                 <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
                   <img class="w-full h-full object-cover" src="img/user3.jpg" alt="">
